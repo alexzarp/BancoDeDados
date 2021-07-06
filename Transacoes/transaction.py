5fad49d3bc7a821de8c2f5e798d2314ccca18e01
alexzarp <alexszds@gmail.com>
2021-07-06 16:46:30 -0300

    Up

--- Transacoes/transaction.py
+++ Transacoes/transaction.py
@@ -3,9 +3,10 @@
 # Trate a exceção no caso  de uma inserção de uma chave já existente. 
 import psycopg2
 conn = None
+id
 try:
     conn = psycopg2.connect(
-        database="atividade",
+        database = "atividade",
         user = "postgres",
         password = "741258",
         host = "127.0.0.1",
@@ -13,23 +14,40 @@ try:
     )
 
     n = int(input("Quantas inserções fazer? "))
-    vetor = []
+    # vetor = []
+    
+    cur = conn.cursor()
     for i in range(n):
         id = int(input("Qual o id desta pessoa? "))
         nome = str(input("Qual o nome desta pessoa? "))
         cpf = int(input("Qual seu CPF? "))
         ender = str(input("O logradouro? "))
-        vetor.append([id, nome, cpf, ender])
-
-    cur = conn.cursor()
-    for i in range(n):
-        id = vetor[i][0]
-        nome = vetor[i][1]
-        cpf = vetor[i][2]
-        ender = vetor[i][3]
         cur.execute("insert into cliente values (%s, %s, %s, %s)", (id, nome, cpf, ender))
+        # vetor.append([id, nome, cpf, ender])
     conn.commit()
     cur.close()
+
+    cur = conn.cursor()
+    cur.execute("select * from cliente")
+    rows = cur.fetchall()
+    cur.execute("select count(*) from cliente")
+    count = cur.fetchall()
+    count = count[0][0]
+    cur.close()
+    
+    print("A tabela escontra-se da seguinte forma:\n")
+    print("═════════════════════════════════")
+    for i in range(count):
+        print("Tupla ", i)
+        print("id: ", rows[i][0])
+        print("Nome: ", rows[i][1])
+        print("CPF: ", rows[i][2])
+        print("Logradouro: ", rows[i][3])
+        print("═════════════════════════════════") 
+
+except psycopg2.errors.lookup("23505"):
+    print("O id", id,"não pode ser inserido pois já existe")
+    print("Todas as inserções foram canceladas!")
 
 except psycopg2.DatabaseError as error:
     print(error)
