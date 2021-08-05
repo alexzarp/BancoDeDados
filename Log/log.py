2074288f115ee45d682e0fa1344d5f368788586a
alexzarp <alexszds@gmail.com>
2021-08-04 22:32:41 -0300

    Up

--- Log/log.py
+++ Log/log.py
@@ -28,7 +28,21 @@ try:
         elif transaction_pattern(log[save]):
             cur.execute("select * from dados where id = %s", (log[save][4]))
             rows = cur.fetchall()
-            
+            # print(rows)
+            dataLog = ""
+            if log[save][6] == 'A':
+                dataLog = log[save][8] + log[save][9]
+                if rows[0][1] != dataLog:
+                    cur.execute("update dados set b = %s where id = %s"), (dataLog, log[save][4])
+                    dataLog = log[save][1] + log[save][2]
+                    redo.append(dataLog)
+            else: # B
+                dataLog = log[save][8] + log[save][9]
+                if rows[0][2] != dataLog:
+                    cur.execute("update dados set b = %s where id = %s"), (dataLog, log[save][4])
+                    dataLog = log[save][1] + log[save][2]
+                    redo.append(dataLog)   
+        
         save+=1
 
 except psycopg2.DatabaseError as error:
